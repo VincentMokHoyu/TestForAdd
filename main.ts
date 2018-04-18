a826d5327d421d7a15ecaad7cdccc48d46c2fce2
VincentMokHoyu <38488551+VincentMokHoyu@users.noreply.github.com>
2018-04-18 15:26:09 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 //% weight=0 color=#3CB371 icon="\uf0ad" block="Tools"
 namespace tools {
     /**
-    * ­pºâªø¤è§Î­±¿n¡A¨Ã¦^¶Ç
+    * è¨ˆç®—é•·æ–¹å½¢é¢ç©ï¼Œä¸¦å›žå‚³
     */
     //% blockId="areaOfRectangle" block="area of rectangle length %length|width %width"
     //% blockGap=2 weight=0 blockExternalInputs=true
@@ -9,11 +9,24 @@ namespace tools {
         return length*width
     }
     /**
-    * ­pºâªø¤è§Î­±¿n¡A¤£¦^¶Ç¡A¥uÅã¥Ü¦bLED
+    * è¨ˆç®—é•·æ–¹å½¢é¢ç©ï¼Œä¸å›žå‚³ï¼Œåªé¡¯ç¤ºåœ¨LED
     */
     //% blockId="ledOfRectangle" block="show area of rectangle length %length|width %width"
     //% blockGap=2 weight=1
     export function ledOfRectangle(length: number, width:number): void {
         basic.showNumber(length*width)
     }
+    
+     //% blockId="CarMoveForward" block="CarMoveForward %Speed"
+    //% blockGap=2 weight=1
+    export function CarMoveForward(Speed: number): void {
+        let buf = pins.createBuffer(5);
+    buf[0] = 6 + 4 * 12
+    buf[1] = 0 & 0xff
+    buf[2] = (0 >> 8) & 0xff
+    buf[3] = 4095 & 0xff
+    buf[4] = (4095 >> 8) & 0xff
+    pins.i2cWriteBuffer(0x41, buf, false)
+    }
+  
 }
