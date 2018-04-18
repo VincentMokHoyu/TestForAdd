4080316bd7f8e96d26915fd2cdb4fe3fa552b5ed
VincentMokHoyu <38488551+VincentMokHoyu@users.noreply.github.com>
2018-04-18 21:43:00 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,24 +1,6 @@
 //% weight=0 color=#3CB371 icon="\uf0ad" block="S3BigCar"
 namespace S3BigCar {
-    /**
-    * 計算長方形面積，並回傳
-    */
-    //% blockId="areaOfRectangle" block="area of rectangle length %length|width %width"
-    //% blockGap=2 weight=0 blockExternalInputs=true
-    export function areaOfRectangle(length: number, width:number): number {
-        return length*width
-    }
-    /**
-    * 計算長方形面積，不回傳，只顯示在LED
-    */
-    //% blockId="ledOfRectangleaaa" block="showeeee area of rectangle length %length|width %width"
-    //% blockGap=2 weight=1
-    export function ledOfRectangleaaa(length: number, width:number): void {
-        basic.showNumber(length*width)
-    }
-    /**
-    * 計算長方形面積，不回傳，只顯示在LED
-    */
+   
     //% blockId="LeftMoveForward" block="LeftMoveForward speed %speed"
     //% blockGap=2 weight=1
     export function LeftMoveForward(speed: number): void {
