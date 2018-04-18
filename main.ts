1a994ae597a572615318ac9d7febf4cc5ed5a1bd
VincentMokHoyu <38488551+VincentMokHoyu@users.noreply.github.com>
2018-04-18 21:03:28 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -19,10 +19,16 @@ namespace tools {
     /**
     * 計算長方形面積，不回傳，只顯示在LED
     */
-    //% blockId="ledOfRectangle2" block="showssss area of rectangle length %length|width %width"
+    //% blockId="ledOfRectangle2" block="CarMove speed %speed"
     //% blockGap=2 weight=1
-    export function ledOfRectangle3(length: number, width:number): void {
-        basic.showNumber(length*width)
-    }
+    export function LeftMoveForward(speed: number): void {
+        let buf = pins.createBuffer(5);
+        buf[0] = 6 + 4 * 12
+        buf[1] = 0 & 0xff
+        buf[2] = (0 >> 8) & 0xff
+        buf[3] = 4095 & 0xff
+        buf[4] = (4095 >> 8) & 0xff
+        pins.i2cWriteBuffer(0x41, buf, false)        
+        }
   
 }
