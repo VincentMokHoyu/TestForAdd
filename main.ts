efe6e6534986bf0cc69c76ccac39fa14961fcff1
VincentMokHoyu <38488551+VincentMokHoyu@users.noreply.github.com>
2018-04-19 14:29:10 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -7,7 +7,14 @@ namespace S3BigCar {
         buf[1] = value
         pins.i2cWriteBuffer(addr, buf)
     }
+   
+       function i2cread(addr: number, reg: number) {
+        pins.i2cWriteNumber(addr, reg, NumberFormat.UInt8BE);
+        let val = pins.i2cReadNumber(addr, NumberFormat.UInt8BE);
+        return val;
+    }
 
+   
      function initPCA9685(): void {
         i2cwrite(0x41, 0x00, 0x00)
         setFreq(50);
