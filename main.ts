52192544d41a246829792589d76f38f443ef9186
VincentMokHoyu <38488551+VincentMokHoyu@users.noreply.github.com>
2018-04-19 10:27:28 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -4,13 +4,23 @@ namespace S3BigCar {
     //% blockId="LeftMoveForward" block="LeftMoveForward speed %speed"
     //% blockGap=2 weight=1
     export function LeftMoveForward(speed: number): void {
-        let buf = pins.createBuffer(5);
-        buf[0] = 6 + 4 * 12
-        buf[1] = 0 & 0xff
-        buf[2] = (0 >> 8) & 0xff
-        buf[3] = speed & 0xff
-        buf[4] = (speed >> 8) & 0xff
-        pins.i2cWriteBuffer(0x41, buf, false)        
+       mbit_小车类.CarCtrl(mbit_小车类.CarState.Car_Run)
+    mbit_小车类.CarCtrl(mbit_小车类.CarState.Car_Stop)
+    
+let buf = pins.createBuffer(5);
+buf[0] = 6 + 4 * 12
+    buf[1] =  0 & 0xff
+    buf[2] =  (0 >> 8) & 0xff
+    buf[3] =  4095 & 0xff
+    buf[4] =  (4095 >> 8) & 0xff
+    pins.i2cWriteBuffer(0x41, buf, false)
+let buf2 = pins.createBuffer(5);
+buf2[0] = 6 + 4 * 13
+    buf2[1] =  0 & 0xff
+    buf2[2] =  (0 >> 8) & 0xff
+    buf2[3] =  0 & 0xff
+    buf2[4] =  (0 >> 8) & 0xff
+    pins.i2cWriteBuffer(0x41, buf2, false)      
         }
     
     //% blockId="RightMoveForward" block="RightMoveForward speed %speed"
