272c0374772d4bf0b942fc4d5022776ec6f6049f
VincentMokHoyu <38488551+VincentMokHoyu@users.noreply.github.com>
2018-04-19 11:13:27 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -33,20 +33,24 @@ buf2[0] = 6 + 4 * 13
            mbit_小车类.CarCtrl(mbit_小车类.CarState.Car_Run)
     mbit_小车类.CarCtrl(mbit_小车类.CarState.Car_Stop)
     
-let buf = pins.createBuffer(5);
-buf[0] = 6 + 4 * 14
-    buf[1] =  0 & 0xff
-    buf[2] =  (0 >> 8) & 0xff
-    buf[3] =  speedR & 0xff
-    buf[4] =  (speedR >> 8) & 0xff
-    pins.i2cWriteBuffer(0x41, buf, false)
-let buf2 = pins.createBuffer(5);
-buf2[0] = 6 + 4 * 15
-    buf2[1] =  0 & 0xff
-    buf2[2] =  (0 >> 8) & 0xff
-    buf2[3] =  0 & 0xff
-    buf2[4] =  (0 >> 8) & 0xff
-    pins.i2cWriteBuffer(0x41, buf2, false)   
+       let buf3 = pins.createBuffer(5);
+buf3[0] = 6 + 4 * 14
+    buf3[1] =  0 & 0xff
+    buf3[2] =  (0 >> 8) & 0xff
+    buf3[3] =  0 & 0xff
+    buf3[4] =  (0 >> 8) & 0xff
+    pins.i2cWriteBuffer(0x41, buf3, false)
+let buf4 = pins.createBuffer(5);
+buf4[0] = 6 + 4 * 15
+    buf4[1] =  0 & 0xff
+    buf4[2] =  (0 >> 8) & 0xff
+    buf4[3] =  speedR & 0xff
+    buf4[4] =  (speedR >> 8) & 0xff
+    pins.i2cWriteBuffer(0x41, buf4, false)  
+       
+      
+       
+
         }
    
    
@@ -93,19 +97,22 @@ buf2[0] = 6 + 4 * 13
        mbit_小车类.CarCtrl(mbit_小车类.CarState.Car_Run)
     mbit_小车类.CarCtrl(mbit_小车类.CarState.Car_Stop)
     
-let buf = pins.createBuffer(5);
-buf[0] = 6 + 4 * 14
-    buf[1] =  0 & 0xff
-    buf[2] =  (0 >> 8) & 0xff
-    buf[3] =  0 & 0xff
-    buf[4] =  (0 >> 8) & 0xff
-    pins.i2cWriteBuffer(0x41, buf, false)
-let buf2 = pins.createBuffer(5);
-buf2[0] = 6 + 4 * 15
-    buf2[1] =  0 & 0xff
-    buf2[2] =  (0 >> 8) & 0xff
-    buf2[3] =  speedR & 0xff
-    buf2[4] =  (speedR >> 8) & 0xff
-    pins.i2cWriteBuffer(0x41, buf2, false)         
+       let buf3 = pins.createBuffer(5);
+buf3[0] = 6 + 4 * 14
+    buf3[1] =  0 & 0xff
+    buf3[2] =  (0 >> 8) & 0xff
+    buf3[3] =  speedR & 0xff
+    buf3[4] =  (speedR >> 8) & 0xff
+    pins.i2cWriteBuffer(0x41, buf3, false)
+let buf4 = pins.createBuffer(5);
+buf4[0] = 6 + 4 * 15
+    buf4[1] =  0 & 0xff
+    buf4[2] =  (0 >> 8) & 0xff
+    buf4[3] =  0 & 0xff
+    buf4[4] =  (0 >> 8) & 0xff
+    pins.i2cWriteBuffer(0x41, buf4, false)   
+       
+       
+       
         }
   }
